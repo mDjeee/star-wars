cb72971e2dc818da9524d853053a0381005e5317
Сейтниязов Исламбек <i.sejtniyazov@tengebank.uz>
2023-11-22 16:03:58 +0500

    feat: add page titles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,27 +9,33 @@ const routes: Routes = [
   },
   {
     path: 'people',
-    loadChildren: () => import('./pages/people/people.module').then(m => m.PeopleModule)
+    loadChildren: () => import('./pages/people/people.module').then(m => m.PeopleModule),
+    title: 'People',
   },
   {
     path: 'films',
-    loadChildren: () => import('./pages/films/films.module').then(m => m.FilmsModule)
+    loadChildren: () => import('./pages/films/films.module').then(m => m.FilmsModule),
+    title: 'Films',
   },
   {
     path: 'ships',
-    loadChildren: () => import('./pages/ships/ships.module').then(m => m.ShipsModule)
+    loadChildren: () => import('./pages/ships/ships.module').then(m => m.ShipsModule),
+    title: 'Starships',
   },
   {
     path: 'transports',
-    loadChildren: () => import('./pages/transports/transports.module').then(m => m.TransportsModule)
+    loadChildren: () => import('./pages/transports/transports.module').then(m => m.TransportsModule),
+    title: 'Transports',
   },
   {
     path: 'planets',
-    loadChildren: () => import('./pages/planets/planets.module').then(m => m.PlanetsModule)
+    loadChildren: () => import('./pages/planets/planets.module').then(m => m.PlanetsModule),
+    title: 'Planets',
   },
   {
     path: 'nations',
-    loadChildren: () => import('./pages/nations/nations.module').then(m => m.NationsModule)
+    loadChildren: () => import('./pages/nations/nations.module').then(m => m.NationsModule),
+    title: 'Nations',
   },
   {
     path: '',
